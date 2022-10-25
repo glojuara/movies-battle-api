2c016a595ebf428c0fb9e1febb0e62ec4ff72e5a
Guilherme Lopes de Araujo <guilherme.xh@hotmail.com>
2022-10-25 06:40:59 -0300

    configurando sql init distintos

---
+++ src/test/resources/data-test.sql
@@ -0,0 +1,4 @@
+INSERT INTO ADA_MOVIE (id, title, partner, partner_id) VALUES (1, 'Test movie 1', 'omdbapi', 'tt3896198')
+INSERT INTO ADA_MOVIE (id, title, partner, partner_id) VALUES (2, 'Test movie 2', 'omdbapi', 'tt0848228')
+INSERT INTO ADA_MOVIE (id, title, partner, partner_id) VALUES (3, 'Test movie 3', 'omdbapi', 'tt10648342')
+INSERT INTO ADA_MOVIE (id, title, partner, partner_id) VALUES (4, 'Test movie 4', 'omdbapi', 'tt6290396')
